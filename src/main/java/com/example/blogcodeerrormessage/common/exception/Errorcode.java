673d1c212ce2cbf0267e4ca52fe003fae0fc00b2
hyune <choi8608@gmail.com>
2022-06-08 00:29:52 +0900

    [step2] MessageSource로 관리하기

--- src/main/java/com/example/blogcodeerrormessage/common/exception/Errorcode.java
+++ src/main/java/com/example/blogcodeerrormessage/common/exception/Errorcode.java
@@ -1,17 +1,40 @@
 package com.example.blogcodeerrormessage.common.exception;
 
+import java.util.Arrays;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
 @Getter
 @RequiredArgsConstructor
 public enum Errorcode {
 
-	NOT_EXIST_PRODUCT("1001", "대상 상품 없음"),
-	OUT_OF_STOCK("1200", "상품 재고 없음"),
-	UNKNOWN("9999", "알 수 없는 에러"),
+	NOT_EXIST_PRODUCT("1001", "product.not_exist_product"),
+	OUT_OF_STOCK("1200", "product.out_of_stock"),
+	UNKNOWN("9999", "product.unknown"),
 	;
 
 	private final String code;
-	private final String reason;
+	private final String propertiesCode;
+	private String reason;
+
+	@RequiredArgsConstructor
+	@Component
+	public static class ErrorreasonInjector {
+
+		private final MessageSource messageSource;
+
+		@Value("${application.locale}")
+		private String locale;
+
+		@PostConstruct
+		public void postConstruct() {
+			Arrays.stream(Errorcode.values())
+					.forEach(errorcode -> errorcode.reason = messageSource.getMessage(errorcode.getPropertiesCode(), null, Locale.forLanguageTag(this.locale)));
+		}
+	}
 }
