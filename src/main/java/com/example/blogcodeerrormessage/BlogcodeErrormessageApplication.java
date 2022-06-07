c3873752677daece4b26d99131732b17d07fe44a
hyune <choi8608@gmail.com>
2022-06-07 23:24:38 +0900

    Initial commit

---
+++ src/main/java/com/example/blogcodeerrormessage/BlogcodeErrormessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogcodeerrormessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcodeErrormessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcodeErrormessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogcodeerrormessage/BlogcodeErrormessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogcodeerrormessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogcodeErrormessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
